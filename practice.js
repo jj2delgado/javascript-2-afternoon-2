6ab5232685102e07ac7bf77c89d51233a4cfca9e
jj2delgado <jj2delgado@yahoo.com>
2019-06-10 00:23:42 -0600

    finished main problems but not extra practice problems

--- practice.js
+++ practice.js
@@ -92,7 +92,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 
-
+let evenFinder = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 != 0){
+        arr.splice(i , 1)
+    }
+  }
+  return arr
+}
 
 
 
